927e65ae006dfdb4318a222c0fe9c01013265745
ephedrini <ephedrini@yandex.ru>
2025-01-04 16:41:46 +0300

    feat: set images

--- index.js
+++ index.js
@@ -135,3 +135,36 @@ function getLocalStorage() {
   switchTheme();
 }
 window.addEventListener("load", getLocalStorage);
+
+//portfolio section mode
+
+const portfolioBtns = document.querySelector(".portfolio_btns");
+const portfolioImages = document.querySelectorAll(".portfolio_pic");
+
+portfolioBtns.addEventListener("click", updatePortfolioSection);
+
+function updatePortfolioSection(event) {
+  if (event.target.classList.contains("portfolio_btn")) {
+    const activeBtn = document.querySelector(".btn_active");
+    const pressedBtn = event.target;
+    activeBtn.classList.remove("btn_active");
+    pressedBtn.classList.add("btn_active");
+    let season = event.target.dataset.season;
+    portfolioImages.forEach(
+      (img, index) =>
+        (img.src = `./assets/img/portfolio/${season}/${index + 1}.jpg`)
+    );
+  }
+}
+
+function preloadImages() {
+  const seasons = ["winter", "spring", "summer", "autumn"];
+  seasons.forEach((elem) => {
+    for (let i = 1; i <= 6; i++) {
+      const img = new Image();
+      img.src = `./assets/img/portfolio/${elem}/${i}.jpg`;
+    }
+  });
+}
+
+preloadImages();
