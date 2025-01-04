31a5823b4e161b071227629c4a98182d171daf81
ephedrini <ephedrini@yandex.ru>
2025-01-04 16:27:41 +0300

    feat: add translation

--- index.js
+++ index.js
@@ -25,3 +25,43 @@ window.onload = function () {
       .setAttribute("poster", "assets/img/video-player.jpg");
   }
 };
+
+//language-mode
+let lang = "en";
+
+const langBtns = document.querySelectorAll(".lang");
+
+langBtns.forEach((btn) => btn.addEventListener("click", switchLang));
+
+function switchLang(event) {
+  lang = event.target.textContent;
+  getTranslate(lang);
+}
+
+function getTranslate(lang) {
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const attr = element.getAttribute("data-i18n");
+    const value = i18Obj[lang][attr];
+
+    if (attr.includes("placeholder")) {
+      element.setAttribute("placeholder", value);
+    } else {
+      element.textContent = value;
+    }
+  });
+  langBtns.forEach((btn) => {
+    if (btn.textContent === lang) {
+      btn.classList.add("active");
+    } else {
+      btn.classList.remove("active");
+    }
+  });
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    if (lang === "ru") {
+      document.querySelector(".hero_text").classList.add("small_margin");
+    } else {
+      document.querySelector(".hero_text").classList.remove("small_margin");
+    }
+  }
+}
