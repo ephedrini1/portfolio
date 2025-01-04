9658092d6ca18e239f2efa19ff0c3b40b2cdf0c7
ephedrini <ephedrini@yandex.ru>
2025-01-04 16:24:20 +0300

    feat: add header hide

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+import i18Obj from "./assets/js/translate.js";
+
+window.addEventListener("scroll", hideHeader);
+
+window.onresize = (event) => document.location.reload(true);
+
+function hideHeader() {
+  const header = document.querySelector(".header");
+  window.onscroll = () => {
+    if (
+      window.pageYOffset > 30 &&
+      !window.matchMedia("(max-width: 768px)").matches
+    ) {
+      header.classList.add("header_hide");
+    } else {
+      header.classList.remove("header_hide");
+    }
+  };
+}
+
 window.onload = function () {
   if (window.matchMedia("(max-width: 768px)").matches) {
     document
